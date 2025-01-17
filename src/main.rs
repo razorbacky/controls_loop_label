487a9e222235d9674d25e5ea0719ec029d700d02
YoonChanryul <razorbacky@gmail.com>
2025-01-17 07:08:06 +0000

    이해완료

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
-/*  */
+// 문제가 해결되지 않을때에는 디버그 기능을 이용해라
+// "루프 라벨로 여러 반복문 사이에 모호함 없애기"
+
 fn main() {
     let mut count = 0;
 
     'counting_up : loop { // 바깥쪽 loop
-        println!("count = {count}"); // #1 최초 1회 카운트를 시작해서 내부 loop로 진행한다.
+        println!("count = {count}"); 
 
         let mut remaining = 10;
 
-        loop { // #2  remaining 을 10으로 시작한다.
+        loop { 
             println!("remaining = {remaining}");
             if remaining == 9 {
-                break; // #4 .3에서 루프를 돌아 9 가 된 remaining 은 여기서에서 break 한다.
+                break; 
             }
             if count == 2 {
-                break 'counting_up; // count가 2가 되면 내부 루프를 해제하고, 외부 루프로 넘어간다.
+                break 'counting_up; // 바깥쪽의 counting_up 루프를 탈출하여 println!("End~~")로 간다."바깥쪽 루프 탈출에 주의"
             }
-            remaining -= 1; // #3 내부 loop가 한 번 돌 때마다 remaining 에서 -1 을 진행한다.
+            remaining -= 1; 
         }
         count += 1;
     }
